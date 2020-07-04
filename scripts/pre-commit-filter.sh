3e86c652ee7c009c09533f346997cd77c1387f2b
Russell Boley <raboley@gmail.com>
2020-07-04 16:36:08 -0700

    said newly created markers

--- scripts/pre-commit-filter.sh
+++ scripts/pre-commit-filter.sh
@@ -3,11 +3,13 @@
 # this would then output
 # matches: [source/index.html.md\|scripts/]
 
-# @todo #1 Add the build badge for PDD. I added 0pdd as a contributor, maybe it will work now?
 # Actually best thing to do is grep the index.html.md file to 
 # search for files that are referenced in that, and then only run it
 # if a matching file in there is updated. (or itself)
 #figuring out if there is a matching path in the file will be harder than I thought probably
+
+# @todo #1 Add the build badge for PDD. I added 0pdd as a contributor, maybe it will work now?
+
 SRC_PATTERN='source/index.html.md\|scripts/'
 git diff --cached --name-only | if grep --quiet "$SRC_PATTERN"
 then
