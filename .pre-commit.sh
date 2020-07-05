462d698548d585ceb4f79e31b3df55229fad47db
Russell Boley <raboley@gmail.com>
2020-07-04 19:49:20 -0700

    Needed to source bash profile since git commit hooks didn't have access to the same path.

--- .pre-commit.sh
+++ .pre-commit.sh
@@ -1,1 +1,6 @@
+source ~/.bash_profile
+# embedme pre-commit hook so that if some script or documentation 
+# was updated to include more scripts the documentation pages get updated
 . ./scripts/pre-commit-filter.sh ". scripts/embedme.sh; git add ./source/index.html.md"
+# Pdd pre-commit hook so that if there is going to a pdd issue it is caught prior to commit.
+. ./scripts/pdd-commit-hook.sh
